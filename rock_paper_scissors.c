c670914919a427471389df0721a1961af408ae08
Mas Renko <masrenko123@gmail.com>
2021-10-04 12:42:22 +0545

    first commit

---
+++ rock_paper_scissors.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int rockpaperscissors(char you, char comp){
+    // returns 1 if you win, -1 if you lose and 0 if draw
+    // Condition for draw
+    // rr
+    // pp
+    // ss
+    if(you == comp){
+        return 0;
+    }
+
+    // Non-draw conditions
+    // rs   
+    // sr
+    // rp 
+    // pr
+    // sp
+    // ps
+    
+
+    if(you=='r' && comp=='s'){
+        return 1;
+    }
+    else if(you=='s' && comp=='r'){
+        return -1;
+    }
+
+    if(you=='r' && comp=='p'){
+        return -1;
+    }
+    else if(you=='p' && comp=='r'){
+        return 1;
+    }
+
+    if(you=='s' && comp=='p'){
+        return 1;
+    }
+    else if(you=='p' && comp=='s'){
+        return -1;
+    }
+
+}
+int main(){
+    char you, comp;
+    srand(time(0));
+    int number = rand()%100 + 1;
+
+    if(number<33){
+        comp = 'r';
+    }
+    else if(number>33 && number<66){
+        comp='p';
+    }
+    else{
+        comp='s';
+    }
+    
+    printf("Enter 'r' for rock, 'p' for paper and 's' for scissors\n");
+    scanf("%c", &you);
+    int result = rockpaperscissors(you, comp);
+    if(result ==0){
+        printf("Game draw!\n");
+    }
+    else if(result==1){
+        printf("You won!\n");
+    }
+    else{
+        printf("You Lose!\n");
+    }
+    printf("You chose %c and computer chose %c. ", you, comp);
+    return 0;
+}
